34bf6640869df2c265d6b4ef526c34cc9f8bf82d
Isaak-Rodrigues <isaakrodrigues241@gmail.com>
2026-09-17 20:35:48 -0300

    Update fuel case statements to be case-insensitive

--- att-10-p.py
+++ att-10-p.py
@@ -14,7 +14,7 @@ valor_alcool = 3.78
 valor_gasolina = 6.59
 
 match combustivel:
-  case 'a':
+  case 'a' | 'A'
     valorA = valor_alcool * litros
     if litros <= 25:
       total = valorA * 0.90
@@ -22,7 +22,7 @@ match combustivel:
       total = valorA * 0.80
     print(f'Valor a pagar no álcool: R$ {total}')
     
-  case 'g':
+  case 'g' | 'G'
     valorG = valor_gasolina * litros
     if litros <= 25:
       total = valorG * 0.85
