484b7b4541a7f7bbcd901f5efad7e039dce44204
Isaak-Rodrigues <isaakrodrigues241@gmail.com>
2026-09-17 20:22:50 -0300

    Fix print formatting and case sensitivity for input

--- att-10-p.py
+++ att-10-p.py
@@ -2,11 +2,11 @@ import os
 
 os.system('cls')
 
-print('Combustível |  	Quantidade Vendida	 |Desconto por Litro')
+print('Combustível |  	Quantidade Vendida	   |Desconto por Litro')
 print('Álcool      |    Até 25 litros	         |     10%')
 print('Álcool	     |   Acima de 25 litros	     |     20%')
-print('Gasolina	 |     Até 25 litros	     |     15%')
-print('Gasolina	 |   Acima de 25 litros      |     30%')
+print('Gasolina	   |     Até 25 litros	       |     15%')
+print('Gasolina    |   Acima de 25 litros      |     30%')
 
 combustivel = str(input('digite (A) para alcool e (G) para gasolina. '))
 litros = int(input('digite quantos litros deseja. '))
@@ -14,18 +14,18 @@ valor_alcool = 3.78
 valor_gasolina = 6.59
 
 match combustivel:
-  case 'a':
+  case 'A':
     valorA = valor_alcool * litros
     if litros <= 25:
-      print (f'valor a pagar no alcool: {valora / 0.10}')
+      print (f'valor a pagar no alcool: {valorA / 0.10}')
     else:
-      print (f'valor a pagar no alcool: {valora / 0.20}')
-  case 'g':
+      print (f'valor a pagar no alcool: {valorA / 0.20}')
+  case 'G':
     valorG = valor_gasolina * litros
     if litros <= 25:
-      print (f'valor a pagar no gasolina: {valorg / 0.15}')
+      print (f'valor a pagar no gasolina: {valorG / 0.15}')
     else:
-      print (f'valor a pagar no gasolina: {valorg / 0.30}')
+      print (f'valor a pagar no gasolina: {valorG / 0.30}')
       
 
 
